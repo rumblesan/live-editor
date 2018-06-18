2a311bc3edd586afbf685c020146b1a8c06ed18c
Guy John <guy@rumblesan.com>
2018-06-18 14:34:35 +0100

    Fixing issues with simple draw lib

--- src/app/simpledraw.js
+++ src/app/simpledraw.js
@@ -23,6 +23,7 @@ export function drawLine(drawLib, x1, y1, x2, y2, colour) {
   } else {
     ctx.strokeStyle = 'black';
   }
+  ctx.beginPath();
   ctx.moveTo(x1 * width, y1 * height);
   ctx.lineTo(x2 * width, y2 * height);
   ctx.stroke();
@@ -34,9 +35,9 @@ export function drawRect(drawLib, x1, y1, x2, y2, colour) {
     canvas: { width, height },
   } = drawLib;
   if (colour) {
-    ctx.strokeStyle = colour;
+    ctx.fillStyle = colour;
   } else {
-    ctx.strokeStyle = 'black';
+    ctx.fillStyle = 'black';
   }
   ctx.fillRect(x1 * width, y1 * height, x2 * width, y2 * height);
 }
